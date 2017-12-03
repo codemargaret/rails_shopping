6360b5659f214c4d06c579dff24060cb9aa8f58d
Margaret Berry <codeberry1@gmail.com>
2017-12-03 14:06:16 -0800

    write and pass unit tests

---
+++ spec/models/order_item_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe OrderItem, 'validation' do
+  it { should validate_presence_of :quantity }
+end
+
+describe OrderItem, 'association' do
+  it { should belong_to :order }
+  it { should belong_to :product }
+end
+
+describe OrderItem, 'column_specification' do
+  it { should have_db_column(:quantity).of_type(:integer) }
+  it { should have_db_column(:product_id).of_type(:integer) }
+  it { should have_db_column(:order_id).of_type(:integer) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
+end
---
+++ spec/models/order_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Order, 'validation' do
+  it { should validate_presence_of :status }
+  it { should validate_presence_of :account_id }
+  it { should validate_presence_of :total_price }
+end
+
+describe Order, 'association' do
+  it { should have_many :order_items }
+end
+
+describe Order, 'column_specification' do
+  it { should have_db_column(:status).of_type(:string) }
+  it { should have_db_column(:account_id).of_type(:integer) }
+  it { should have_db_column(:total_price).of_type(:decimal) }
+  it { should have_db_column(:created_at).of_type(:datetime) }
+  it { should have_db_column(:updated_at).of_type(:datetime) }
+end

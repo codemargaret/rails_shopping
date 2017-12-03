1e7625cadaa462736304a7b0b4c10467df3e38c7
Margaret Berry <codeberry1@gmail.com>
2017-12-03 13:47:14 -0800

    set up model relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  has_many :order_items
+
+  validates :status, :presence => true
+  validates :account_id, :presence => true
+  validates :total_price, :presence => true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  has_many :order_items
+
+  validates :price, :presence => true
+  validates :name, :presence => true
 end

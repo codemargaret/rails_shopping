ff7e65d57d1bc410eb8dfb5b9cbea1d2b888d5cd
Margaret Berry <codeberry1@gmail.com>
2017-12-03 14:48:23 -0800

    fix errors in user unit tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe User, type: :model do
 end
 
 describe User, 'validation' do
-  it { should validate_presence_of :status }
-  it { should validate_presence_of :account_id }
-  it { should validate_presence_of :total_price }
+  it { should validate_presence_of :email }
+  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+  it { should validate_presence_of :username }
+  it { should validate_uniqueness_of :username }
+  it { should validate_presence_of :password }
 end
 
 describe User, 'association' do
@@ -15,9 +17,11 @@ describe User, 'association' do
 end
 
 describe User, 'column_specification' do
-  it { should have_db_column(:status).of_type(:string) }
-  it { should have_db_column(:account_id).of_type(:integer) }
-  it { should have_db_column(:total_price).of_type(:decimal) }
+  it { should have_db_column(:email).of_type(:string) }
+  it { should have_db_column(:username).of_type(:string) }
+  it { should have_db_column(:admin).of_type(:boolean) }
+  it { should have_db_column(:encrypted_password).of_type(:string) }
+  it { should have_db_column(:sign_in_count).of_type(:integer) }
   it { should have_db_column(:created_at).of_type(:datetime) }
   it { should have_db_column(:updated_at).of_type(:datetime) }
 end
